f0082a183c5c8a2fa10f0e813b436b782228e73c
amine <a.aminebella@gmail.com>
2025-04-27 13:49:01 +0100

    display the information related to the products

--- src/app/products/features/product-list/product-list.component.ts
+++ src/app/products/features/product-list/product-list.component.ts
@@ -6,6 +6,9 @@ import { ButtonModule } from "primeng/button";
 import { CardModule } from "primeng/card";
 import { DataViewModule } from 'primeng/dataview';
 import { DialogModule } from 'primeng/dialog';
+import {CurrencyPipe, DatePipe} from "@angular/common";
+import {RatingModule} from "primeng/rating";
+import {FormsModule} from "@angular/forms";
 
 const emptyProduct: Product = {
   id: 0,
@@ -29,7 +32,7 @@ const emptyProduct: Product = {
   templateUrl: "./product-list.component.html",
   styleUrls: ["./product-list.component.scss"],
   standalone: true,
-  imports: [DataViewModule, CardModule, ButtonModule, DialogModule, ProductFormComponent],
+  imports: [DataViewModule, CardModule, ButtonModule, DialogModule, ProductFormComponent, DatePipe, CurrencyPipe, RatingModule, FormsModule],
 })
 export class ProductListComponent implements OnInit {
   private readonly productsService = inject(ProductsService);
